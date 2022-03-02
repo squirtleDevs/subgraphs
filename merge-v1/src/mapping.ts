bfd0a664b94ab3f1d9ecc8a6e53cfbfbaabead98
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-03-02 11:04:38 -0500

    revert changes back in order to create proper PR to main from feature branch with repo changes in these commits

---
+++ merge-v1/src/mapping.ts
@@ -0,0 +1,63 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Merge,
+  AlphaMassUpdate,
+  Approval,
+  ApprovalForAll,
+  ConsecutiveTransfer,
+  MassUpdate,
+  Transfer
+} from "../generated/Merge/Merge"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleAlphaMassUpdate(event: AlphaMassUpdate): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.tokenId = event.params.tokenId
+  entity.alphaMass = event.params.alphaMass
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // None
+}
+
+export function handleApproval(event: Approval): void {}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {}
+
+export function handleConsecutiveTransfer(event: ConsecutiveTransfer): void {}
+
+export function handleMassUpdate(event: MassUpdate): void {}
+
+export function handleTransfer(event: Transfer): void {}
