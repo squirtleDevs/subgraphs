a87b27ffc3fe9d6a806b270b8c3f75156012a06a
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-03-11 17:11:42 -0500

    Update merge subgraph mapping as per DK review for BigInt conversion and start AlphaMassUpdate event handler

--- merge-v1/src/mapping.ts
+++ merge-v1/src/mapping.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 import {
   Merge,
   Transfer,
@@ -11,28 +11,42 @@ import {
 import { User, NFT } from "../generated/schema";
 import { EMPTY_ADDRESS, CLASS_MULTIPLIER } from "./utils";
 
+/* ========== GLOBAL VARIABLES ========== */
+
+//TODO: not sure if we should have global variables?
+
+// either store the alphaId and alphaMass size in a global variable somehow... or we have to handleAlphaMassUpdate() and take the tokenId that alphaMassUpdate gives us, and pass that to an NFT entity.
+// NFT entity takes in the tokenId and updates boolean of whether it is Alpha or not. Prior to an AlphaMassUpdate() event, does Transfer() get called? I think it does.
+
 /* ========== HELPER FUNCTIONS ========== */
 
-// /**
-//  * @notice sort color of nft based on massClass
-//  * @param massClass corresponding to the enum Class from schema
-//  * @returns Color: String
-//  */
-//  function checkColor(mergeClass: String): String {
-//   let color: String;
-//   if (mergeClass == "ONE") return (color = "WHITE");
-//   if (mergeClass == "TWO") return (color = "YELLOW");
-//   if (mergeClass == "THREE") return (color = "BLUE");
-//   if (mergeClass == "FOUR") return (color = "RED");
-// }
+/**
+ * @notice sort color of nft based on massClass
+ * @param massClass corresponding to the enum Class from schema
+ * @returns Color: string
+ */
+function checkColor(mergeClass: string): string {
+  let color: string;
+  color = "WHITE";
+  if (mergeClass == "ONE") {
+    return color;
+  } else if (mergeClass == "TWO") {
+    color = "YELLOW";
+  } else if (mergeClass == "THREE") {
+    color = "BLUE";
+  } else if (mergeClass == "FOUR") {
+    color = "RED";
+  }
+  return color;
+}
 
 /**
  * @notice sort mergeClass as a string to fit schema enum reqs
  * @param _mergeClass
- * @returns mergeClass: String
+ * @returns mergeClass: string
  */
-function checkMergeClass(_mergeClass: BigInt): String {
-  let mergeClass: String;
+function checkMergeClass(_mergeClass: BigInt): string {
+  let mergeClass: string;
   mergeClass = "ONE";
   if (_mergeClass == new BigInt(1)) {
     return mergeClass;
@@ -55,7 +69,7 @@ function checkMergeClass(_mergeClass: BigInt): String {
  * NOTE: Checks through different scenarios (current WIP covers): 1. minting
  * TODO: 2. transfer from NG to ext. addresses, 3. transfers btw ext. addresses to other ext. addresses (not whitelisted), 4. merge() being called, 5. possibly merge being called by NG specifically.
  */
-export function handleTransfer(event: Transfer): void {
+function handleTransfer(event: Transfer): void {
   let _to = event.params.to;
   let _from = event.params.from;
 
@@ -85,6 +99,38 @@ export function handleTransfer(event: Transfer): void {
 
   user.save();
 }
+
+// /**
+//  *
+//  * @param event
+//  * @param user
+//  * @returns
+//  */
+// export function handleAlphaMassUpdate(event: AlphaMassUpdate): void {
+//   let alphaTokenId = event.params.tokenId;
+//   let alphaMass = event.params.alphaMass;
+
+//   // so basically, alpha updates only during mint() if tokenID != oldAlphaTokenID && during _merge().
+
+//   // in the event of _merge(), there are a couple scenarios where this happens.
+//   // merge(): when _transfer() is called, the actual ownership is done in the same tx. This is a key thing to start getting used to for subgraphs. When an event happens, unless the params have something to do with the tx at that literal point in time, then as long as the relevant variables are updated within the same tx of the event being emitted, we will be good!
+
+//   // OK, I get the tokenId, then I call up load.nft(tokenId), and from there I update nft.isAlpha. The cool thing is that I won't have to check with the transferHandler() if tokens are alpha because this event takes care of it. If it did not take care of it, then I would have to figure out a way to compare what tokenId is the alpha and its associated values. I could do that through the generic entity for the collection. I don't have that set up yet though and since I have this event emission I'm good.
+
+//   //within same tx, token owners and all that have been updated. Transfer() event will have been emitted.
+//   let _alphaTokenId = alphaTokenId.toString();
+//   let nft = NFT.load(_alphaTokenId);
+
+//   // QUESTION for mint(): do subgraphs get the event data in the order that it happens within the smart contract logic? I ask this bc within mint() the Transfer() event happens before the AlphaMassUpdate() event. since it would be in the same tx, then I just want to know if I need to put logic here that checks if the nft exists or not. The only reason it wouldn't exist during mint() is if the Transfer() event didn't get digested first by the subgraph.
+//   // 1. Transfer() emitted, then AlphaMassUpdate() emitted.
+
+//   // for _merge(): Transfer(from, to, tokenId) emitted, _merge() then called so AlphaMassUpdate(_alphaId, combinedMass) and MassUpdate(tokenIdSmall, tokenIdLarge, combinedMass) emitted, then Transfer(to, address(0), deadTokenId) emitted, then that's it.
+//   if (!nft) {
+//   }
+//   nft.massSize = alphaMass;
+
+//   nft.isAlpha = true;
+// }
 
 /* ========== GENERAL FUNCTIONS ========== */
 
@@ -139,22 +185,29 @@ function createNFT(event: Transfer, user: User): NFT {
   let nftValue = contract.getValueOf(tokenId);
 
   // obtain nft fields throughs calculating off of encoded value
+  log.info("nft value: {}", [nftValue.toString()]);
+  log.info("class multiplier: {}", [CLASS_MULTIPLIER.toString()]);
+  nft.massValue = nftValue;
+  nft.massSize = (nftValue % CLASS_MULTIPLIER) as BigInt; //TODO: not sure why there are error lines. It builds fine.
+  let fun: BigInt = nft.massSize as BigInt;
+  log.info("mass size: {}", [fun.toString()]);
 
-  nft.massSize = nftValue % CLASS_MULTIPLIER; //TODO: not sure why there are error lines. It builds fine.
-
-  let _mergeClass = nftValue / CLASS_MULTIPLIER;
+  let _mergeClass: BigInt = nftValue.div(CLASS_MULTIPLIER);
 
   nft.mergeClass = checkMergeClass(_mergeClass);
+  log.info("merge class: {}", [nft.mergeClass as string]);
+
+  let mergeClass: string = checkMergeClass(_mergeClass);
 
   // let alpha = contract._alphaId();
 
   // if (nft.mergeClass == "FOUR" && tokenId == alpha) {
   //   nft.color = "BLACK";
   // } else {
-  //   nft.color = checkColor(nft.mergeClass);
+  nft.color = checkColor(mergeClass);
   // }
 
-  let mergeCount = 0; //only time mergeCount for an NFT updates is when _merge() or batchSetMergeCountFromSnapshot() is called. The former is what is called post-mint-phase.
+  // let mergeCount = 0; //only time mergeCount for an NFT updates is when _merge() or batchSetMergeCountFromSnapshot() is called. The former is what is called post-mint-phase.
 
   // NOTE: transfer() is only called: 1. in merge() after _merge() is called 2. in _transfer() when to == _dead, two emits happen there. OR when transferring to another address AND/OR when token _merged into another tokenId (after transfer) --> thus taking care of the transfer event of sending a tokenId to address(0). Recall that _transfer() really  just ends up changing the record of ownership of the digital asset in the smart contract.
 
@@ -195,10 +248,9 @@ export function handleMassUpdate(event: MassUpdate): void {}
  */
 export function handleAlphaMassUpdate(event: AlphaMassUpdate): void {}
 
-//  /**
-//   * EVENTS to import as subgraph is iteratively developed: AlphaMassUpdate,
-//   Approval,
-//   ApprovalForAll,
-//   ConsecutiveTransfer,
-//   MassUpdate,
-//   */
+// //  /**
+// //   * EVENTS to import as subgraph is iteratively developed: AlphaMassUpdate,
+// //   Approval,
+// //   ApprovalForAll,
+// //   ConsecutiveTransfer,
+// //   MassUpdate,
--- merge-v1/src/utils.ts
+++ merge-v1/src/utils.ts
@@ -1,4 +1,4 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 export const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
-export const CLASS_MULTIPLIER = new BigInt(100000000);
+export const CLASS_MULTIPLIER = BigInt.fromI32(100000000);
